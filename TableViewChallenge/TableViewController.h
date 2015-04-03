ae1000578f27b9e786886f121218c17b7eac0895
Kurt Henthorn <henthorn.kurt@gmail.com>
2015-04-03 14:42:34 -0400

    tableview

--- TableViewChallenge/TableViewController.h
+++ TableViewChallenge/TableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  TableViewChallenge
 //
 //  Created by Kurt Henthorn on 4/3/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
